9e6030c52fc47c8dd2d778563a5590d64563918c
jhness99 <jhness99@naver.com>
2024-09-26 11:10:50 +0900

    modify test03.py by user01

--- test03.py
+++ test03.py
@@ -1,7 +1,9 @@
 
 
 def sayHello(name):
-    return 'Hello, ' + name
+    return 'Hello, ' + name + '!!' #user01 수정
 
 inName = 'Tom'
 print(sayHello(inName))
+
+print('Requested by boss') #user01 추가
